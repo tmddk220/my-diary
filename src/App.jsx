cf088f3d28319f55fb5fa42478018e72adaabad3
tmddk220 <leeseunga1590@gmail.com>
2024-11-20 17:39:58 +0900

    라우팅 및 index.css 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Review from "./pages/Review";
+import Notfound from "./pages/Notfound";
 
 function App() {
-  return <>다이어리</>;
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/diary"}>Diary</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/review"}>Review</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Review.jsx
@@ -0,0 +1,5 @@
+const Review = () => {
+  return <div>Review 페이지입니다.</div>;
+};
+
+export default Review;
